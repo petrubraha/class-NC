4eaefc9594899b5c110398b6adbb0e81bb2d79d1
Adrian <ionicheadrian@yahoo.com>
2026-02-25 14:09:31 +0200

    rez ex 2

--- lab01/sol.py
+++ lab01/sol.py
@@ -1,3 +1,7 @@
+import time
+import random
+
+
 UNIT = 1.0
 """Global float constant used internally."""
 
@@ -37,7 +41,26 @@ st = (x + y) + z    # (1.0 + u/10) + u/10
 dr = x + (y + z)    # 1.0 + (u/10 + u/10)
 
 if st!=dr:
-    print(f"(x + y) + z = {left}")
-    print(f"x + (y + z) = {right}")
+    print(f"(x + y) + z = {st}")
+    print(f"x + (y + z) = {dr}")
     print(f"Neasociativ  {st!=dr}")
 # acum trebuie sa gasim x y z pentru care inmultirea ESTE ASOCIATIVA
+
+s=time.time()
+for incercare in range(1,100_001):
+    x = 10 ** random.uniform(40, 200)
+    y = 10 ** random.uniform(-180, -100)
+    z = 10 ** random.uniform(-155, -90)
+    st = (x * y) * z
+    dr = x * (y * z)
+
+    if st != dr and st > 0 and dr == 0.0:
+        print(f"Gasit dupa {incercare} incercari \n x = {x:.2e}, y = {y:.2e}, z = {z:.2e}")
+        print(f"(x*y)*z = {st:.2e}")
+        print(f"x*(y*z) = {dr}")
+        break
+
+print(f"Timp total {time.time()-s:.4f} secunde")
+
+     
+
